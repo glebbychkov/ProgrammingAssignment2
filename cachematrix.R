4a17127fdc9d35649cb67ccf8145e56f855e53b8
zetgleb <zetglebzet@gmail.com>
2017-08-25 16:27:59 +0300

    ComputingAssighment2
    A little bit late, but I hope it will be graded

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Written functions in the code compute the inverse matrix
+## 
 
-## Write a short comment describing this function
+## The argument of the function should be
+## nonsingular, nondegenerate and squared matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+      x <<- y
+      m <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) m <<- solve
+    getsolve <- function() m
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
-
+## The argument of the function is the list computed by
+## using makeCacheMatrix()
 cacheSolve <- function(x, ...) {
+    m <- x$getsolve()
+    if(!is.null(m)) {
+      message("getting cached data")
+      return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setsolve(m)
+    m
         ## Return a matrix that is the inverse of 'x'
 }
